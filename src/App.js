6ba8d96d660e1be4402568453dc911d4f309226b
Jose Medina <yanoya10@hotmail.com>
2020-05-16 01:16:38 -0400

    added logic and functionality, need to fix images problem

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Employee from './components/Employee';
+import Button from './components/Button';
+import employees from './employees.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    employees: employees
+  };
+
+  handleInputChange = event => {
+    // Getting the value and name of the input which triggered the change
+    const { name, value } = event.target;
+
+    // Updating the input's state
+    this.setState({
+      [name]: value
+    });
+  };
+
+  sortEmployees = (a, b) => {
+    console.log(a.name);
+    const personA = a.name.toUpperCase();
+    const personB = b.name.toUpperCase();
+
+    let comparison = 0;
+    if (personA > personB) {
+      comparison = 1;
+    } else if (personA < personB) {
+      comparison = -1;
+    }
+    console.log(comparison);
+    return comparison;
+  }
+
+
+  filterEmployees = () => {
+    return this.state.employees.filter(employee => employee.department === "Tennis Player")
+  }
+
+
+  render() {
+    return (
+      <>
+        <Button onClick={() => this.setState({employees: this.state.employees.sort(this.sortEmployees)})}>Sort</Button>
+        <Button onClick={() => this.setState({employees: this.filterEmployees()})}>Find All Tennis Player</Button>
+        <Employee employees={this.state.employees} />
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Employee/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Card from '../Card';
+
+class Employee extends Component {
+    render() {
+        return (
+            <div className="employee">
+                {this.props.employees.map(employee => (
+                    <Card key={employee.id} name={employee.name} role={employee.role} department={employee.department} image={employee.image}/>
+                ))}
+            </div>
+        )
+    };
+};
+
+export default Employee;
